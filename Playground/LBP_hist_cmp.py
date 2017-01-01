21fc1e212ad64eef5fed8607fe957a7fea32c1be
efforia <tualet00@gmail.com>
2017-01-01 15:36:27 +0200

    WIP

--- Playground/LBP_hist_cmp.py
+++ Playground/LBP_hist_cmp.py
@@ -1,8 +1,7 @@
 import numpy as np
+import cv2
 import matplotlib.pyplot as plt
-from skimage.transform import rotate
 from skimage.feature import local_binary_pattern
-from skimage import data
 
 
 def hist(ax, lbp):
@@ -17,21 +16,15 @@ def kullback_leibler_divergence(p, q):
     return np.sum(p[filt] * np.log2(p[filt] / q[filt]))
 
 
-def match(refs, img):
+def match(lbp_obj, window):
     best_score = 10
-    best_name = None
-    lbp = local_binary_pattern(img, n_points, radius, METHOD)
+    lbp = local_binary_pattern(window, n_points, radius, METHOD)
     n_bins = lbp.max() + 1
     hist, _ = np.histogram(lbp, normed=True, bins=n_bins, range=(0, n_bins))
-    for name, ref in refs.items():
-        ref_hist, _ = np.histogram(ref, normed=True, bins=n_bins,
-                                   range=(0, n_bins))
-        score = kullback_leibler_divergence(hist, ref_hist)
-        print("Name is {}, Score = {}".format(name, score))
-        if score < best_score:
-            best_score = score
-            best_name = name
-    return best_name
+    ref_hist, _ = np.histogram(lbp_obj, normed=True, bins=n_bins, range=(0, n_bins))
+    score = kullback_leibler_divergence(hist, ref_hist)
+    print("Score is {}".format(score))
+    return score
 
 METHOD = 'uniform'
 plt.rcParams['font.size'] = 9
@@ -40,42 +33,20 @@ plt.rcParams['font.size'] = 9
 radius = 2
 n_points = 8 * radius
 
-brick = data.load('brick.png')
-grass = data.load('grass.png')
-wall = data.load('rough-wall.png')
+obj = cv2.imread("/home/efforia/PycharmProjects/OpenCVProject/images/grant.jpg", 0)
 
-refs = {
-    'brick': local_binary_pattern(brick, n_points, radius, METHOD),
-    'grass': local_binary_pattern(grass, n_points, radius, METHOD),
-    'wall': local_binary_pattern(wall, n_points, radius, METHOD)
-}
+lbp_obj = local_binary_pattern(obj, n_points, radius, METHOD)
 
-# classify rotated textures
-print('Rotated images matched against references using LBP:')
-print('original: brick, rotated: 30deg, match result: ',
-      match(refs, rotate(brick, angle=30, resize=False)))
-print('original: brick, rotated: 70deg, match result: ',
-      match(refs, rotate(brick, angle=70, resize=False)))
-print('original: grass, rotated: 145deg, match result: ',
-      match(refs, rotate(grass, angle=145, resize=False)))
+window = cv2.imread("/home/efforia/PycharmProjects/OpenCVProject/images/jurassicpark.jpg", 0)
 
-# plot histograms of LBP of textures
-fig, ((ax1, ax2, ax3), (ax4, ax5, ax6)) = plt.subplots(nrows=2, ncols=3,
-                                                       figsize=(9, 6))
-plt.gray()
+score = match(lbp_obj, window)
 
-ax1.imshow(brick)
-ax1.axis('off')
-hist(ax4, refs['brick'])
-ax4.set_ylabel('Percentage')
-
-ax2.imshow(grass)
-ax2.axis('off')
-hist(ax5, refs['grass'])
-ax5.set_xlabel('Uniform LBP values')
-
-ax3.imshow(wall)
-ax3.axis('off')
-hist(ax6, refs['wall'])
+# fig, ax = plt.figure()
+# plt.gray()
+#
+# ax.imshow(image)
+# ax.axis('off')
+# hist(ax, )
+# ax.set_ylabel('Percentage')
 
 plt.show()
